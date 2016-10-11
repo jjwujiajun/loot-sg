0f971e83f68f6814f62738bd1caae9c4983ec85c
Jiajun <jj@liquor.com>
2016-10-11 23:33:45 +0800

    Added validation mechanism to PB url field

--- js/putBom.js
+++ js/putBom.js
@@ -1,16 +1,43 @@
 var app = angular.module('putBom', []);
 
 app.controller('putBomController', function($scope) {
-	$scope.urlField = '';
+    $scope.urlField = {
+    	'text': ''
+    };
 	$scope.cartCount = 0;
 
 	$scope.scrapeURL = function () {
 		// Check if input is valid url
-		if ($scope.urlField.length > 0) { 
+		if (isValidURL($scope.urlField.text)) { 
 			// try scrape from import.io
 			if (true) { //scrapped) {
-				$scope.cartCount++;;
+				$scope.cartCount++;
+				addItemToHoverCart();
 			};
 		}
 	}
+
+	$scope.minusCartCount = function () {
+		if ($scope.cartCount > 0){
+			$scope.cartCount--;
+		}
+	}
 });
+
+function addItemToHoverCart() {
+
+}
+
+
+function isValidURL(str) {
+	console.log('test');
+	if (str.indexOf('amazon.com') != -1) {
+		console.log('true1');
+		return true;
+	} else if (str.indexOf('forever21.com') != -1) {
+		console.log('true2');
+		return true;
+	};
+	console.log('false');
+	return false;
+}
