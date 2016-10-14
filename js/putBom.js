a678e038335f8fa5fec99de0a23261fc6330414c
Jiajun <jj@liquor.com>
2016-10-14 14:17:33 +0800

    Added CSS formatting to putBom elements

--- js/putBom.js
+++ js/putBom.js
@@ -1,7 +1,22 @@
 var app = angular.module('putBom', []);
 
 app.service('data', function() {
-	this.items    = [];
+	this.items    = [{
+		number: 1, // temp
+        name: 'Contemporary Floral Print Dress',
+        url: 'http://forever21.com/blah/?blah=1#hello',
+        quantity: '1',
+        size: '',
+        color: '',
+        instructions: '',
+        proceedOrder: true,
+        listPrice: '22.90',
+        imageUrl: './test-img.jpg',
+        sizes: ['S', 'M', 'L'],
+        colors: ['Cream', 'Green'],
+        details: '',
+        shouldShowDeleteBtn: false
+	}];
 
 	this.userInfo = {
 		firstName: '',
@@ -31,8 +46,7 @@ app.controller('putBomController', ['data', '$scope', '$http', function(data, $s
 		if (isValidURL($scope.urlField.text)) { 
 
 			// try scrape from import.io
-			$scope.createEmptyItem();
-			$scope.scrapeF21($scope.urlField.text, data.items[0]);
+			$scope.scrapeF21($scope.urlField.text);
 		}
 	}
 
@@ -41,7 +55,7 @@ app.controller('putBomController', ['data', '$scope', '$http', function(data, $s
 	        number: data.items.length + 1,
 	        name: '',
 	        url: '',
-	        quantity: '',
+	        quantity: '1',
 	        size: '',
 	        color: '',
 	        instructions: '',
@@ -59,7 +73,7 @@ app.controller('putBomController', ['data', '$scope', '$http', function(data, $s
 	// Parameters: 
 	// - url to be passed
 	// - item whos data is to be updated by url
-	$scope.scrapeF21 = function(url, item) {
+	$scope.scrapeF21 = function(url) {
 		var urlString = "https://api.import.io/store/connector/7525a0ab-c857-4f60-8c23-73eb625083de/_query?input=webpage/url:" + encodeURIComponent(url) + "&&_apikey=b34ce8b353894e91b3ef33342f0c5ddb82cce3b3dd7be5b65977ed3fd532f3521d5f3c08c232bafdcc60a719fe799b1b03a95e181771f5bf511f85950dcb7c132b1575addd5fa8c5eeb70645857f693c";
 	    console.log('scrapeF21 GET: ' + urlString);
 	    $http({
@@ -67,15 +81,19 @@ app.controller('putBomController', ['data', '$scope', '$http', function(data, $s
 	        url    : urlString
 	    }).then(function (result) {
 	    	return result.data;
-	    }).then(function (data) {
-	    	var result = data.results[0];
-	    	item.name = result.item_name;
+	    }).then(function (resultData) {
+	    	$scope.createEmptyItem();
+
+	    	var result = resultData.results[0];
+	    	data.items[0].name = result.item_name;
+	    	data.items[0].url = url;
+	    	data.items[0].details = result.details;
 
 	    	// Price
 	    	if(result.price_sale) {
-	            item.listPrice = result.price_sale;
+	            data.items[0].listPrice = result.price_sale;
 	        } else if(result.price_normal) {
-	            item.listPrice = result.price_normal;
+	            data.items[0].listPrice = result.price_normal;
 	        }
 	        
 	        console.log(result.price_sale);
@@ -83,37 +101,42 @@ app.controller('putBomController', ['data', '$scope', '$http', function(data, $s
 
 	        // Image
 	        if(result.image1) {
-	            item.imageUrl  = result.image1;
+	            data.items[0].imageUrl  = result.image1;
 	        } else if(result.image2) {
-	            item.imageUrl  = result.image2;
+	            data.items[0].imageUrl  = result.image2;
 	        } else if(result.image3) {
-	            item.imageUrl  = result.image3;
+	            data.items[0].imageUrl  = result.image3;
 	        }
-	        
+
 	        // Sizes
-	        item.details    = result.details;
 	        if(!Array.isArray(result.sizes_available)) {
-	            item.sizes  = [result.sizes_available];
+	            data.items[0].sizes  = [result.sizes_available];
 	        } else {
-	            item.sizes      = result.sizes_available;    
+	            data.items[0].sizes = result.sizes_available;    
 	        }
 	        
 	        // Colors
 	        if(!Array.isArray(result["colors_available/_alt"])) {
-	            item.colors = [result["colors_available/_alt"]];
+	            data.items[0].colors = [result["colors_available/_alt"]];
 	        } else {
-	            item.colors     = result["colors_available/_alt"];    
+	            data.items[0].colors = result["colors_available/_alt"];    
 	        }
 
-	        // Add to cart
-	        $scope.cartCount++;
 			$scope.urlField.text = '';
 	    });
 	};
 
 	$scope.removeItem = function(itemNumber) {
-		index = items.length - itemNumber;
+		index = data.items.length - itemNumber;
 		data.items.splice(index, 1);
+	}
+
+	$scope.selectColorForItem = function(color, item) {
+		item.color = color;
+	}
+
+	$scope.selectSizeForItem = function(size, item) {
+		item.size = size;
 	}
 
 }]);
