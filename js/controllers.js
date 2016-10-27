77702fe19fd83e3bc1597bda2c98976f5a312813
Will Ho <ikaruswill.ho@gmail.com>
2016-10-27 16:17:57 +0800

    Integrated scrape

--- js/controllers.js
+++ js/controllers.js
@@ -55,47 +55,88 @@ mod.service('data', function() {
 });
 
 
-mod.service('utility', ['$http', function($http) {    
-    this.scrapeF21 = function(url, item) {
-        var urlString = "https://api.import.io/store/connector/7525a0ab-c857-4f60-8c23-73eb625083de/_query?input=webpage/url:" + encodeURIComponent(url) + "&&_apikey=b34ce8b353894e91b3ef33342f0c5ddb82cce3b3dd7be5b65977ed3fd532f3521d5f3c08c232bafdcc60a719fe799b1b03a95e181771f5bf511f85950dcb7c132b1575addd5fa8c5eeb70645857f693c";
-        console.log('scrapeF21 GET: ' + urlString);
-        $http({
-            method : 'GET',
-            url    : urlString
-        }).success(function(data) {
-            var result         = data.results[0];
-            item.name          = result.item_name;
-            if(result.price_sale) {
-                item.listPrice = result.price_sale;
-            } else if(result.price_normal) {
-                item.listPrice = result.price_normal;
-            }
-            
-            console.log(result.price_sale);
-            console.log(result.price_normal);
-            if(result.image1) {
-                item.imageUrl  = result.image1;
-            } else if(result.image2) {
-                item.imageUrl  = result.image2;
-            } else if(result.image3) {
-                item.imageUrl  = result.image3;
-            }
-            
-            item.details    = result.details;
-            if(!Array.isArray(result.sizes_available)) {
-                item.sizes  = [result.sizes_available];
-            } else {
-                item.sizes      = result.sizes_available;    
-            }
-            
-            if(!Array.isArray(result["colors_available/_alt"])) {
-                item.colors = [result["colors_available/_alt"]];
-            } else {
-                item.colors     = result["colors_available/_alt"];    
-            }
-            
-        });
-    };
+mod.service('utility', ['$http', 'data', function($http, data) {
+	var createEmptyItem = function() {
+		this.newItem = {
+	        number: data.items.length + 1,
+	        name: '',
+	        url: '',
+	        quantity: '1',
+	        size: '',
+	        color: '',
+	        instructions: '',
+	        proceedOrder: true,
+	        listPrice: '',
+	        imageUrl: '',
+	        sizes: [],
+	        colors: [],
+	        details: ''
+	    };
+	    data.items.unshift(this.newItem);
+	    console.log(this.newItem.number);
+	};
+
+    this.scrapeF21 = function(url) {
+		var urlString = "https://api.import.io/store/connector/7525a0ab-c857-4f60-8c23-73eb625083de/_query?input=webpage/url:" + encodeURIComponent(url) + "&&_apikey=b34ce8b353894e91b3ef33342f0c5ddb82cce3b3dd7be5b65977ed3fd532f3521d5f3c08c232bafdcc60a719fe799b1b03a95e181771f5bf511f85950dcb7c132b1575addd5fa8c5eeb70645857f693c";
+	    console.log('scrapeF21 GET: ' + urlString);
+	    $http({
+	        method : 'GET',
+	        url    : urlString
+	    }).then(function (result) {
+	    	return result.data;
+	    }).then(function (resultData) {
+	    	// For multiple cart items. Remove this.
+	    	data.items.shift();
+	    	/********/
+
+	    	createEmptyItem();
+
+	    	var result = resultData.results[0];
+	    	data.items[0].name = result.item_name;
+	    	data.items[0].url = url;
+	    	data.items[0].details = result.details;
+
+	    	// Price
+	    	if(result.price_sale) {
+	            data.items[0].listPrice = result.price_sale;
+	        } else if(result.price_normal) {
+	            data.items[0].listPrice = result.price_normal;
+	        }
+	        
+	        console.log(result.price_sale);
+	        console.log(result.price_normal);
+
+	        // Image
+	        if(result.image1) {
+	            data.items[0].imageUrl  = result.image1;
+	        } else if(result.image2) {
+	            data.items[0].imageUrl  = result.image2;
+	        } else if(result.image3) {
+	            data.items[0].imageUrl  = result.image3;
+	        }
+
+	        // Sizes
+	        if(!Array.isArray(result.sizes_available)) {
+	            data.items[0].sizes  = [result.sizes_available];
+	        } else {
+	            data.items[0].sizes = result.sizes_available;    
+	        }
+	        
+	        // Colors
+	        if(!Array.isArray(result["colors_available/_alt"])) {
+	            data.items[0].colors = [result["colors_available/_alt"]];
+	        } else {
+	            data.items[0].colors = result["colors_available/_alt"];    
+	        }
+
+	        if (isNaN(data.items[0].sizes[0])) {
+	        	data.items[0].useCircleForSizes = data.items[0].sizes[0].length <= 2;
+	        } else {
+	        	data.items[0].useCircleForSizes = data.items[0].sizes[0].toString().length <= 2;
+	        }
+	        console.log(data.items[0].useCircleForSizes);
+	    });
+	};
 
 	this.sendOrderEmail = function(data) {
 		//console.log(data.userInfo);
@@ -186,7 +227,7 @@ mod.service('utility', ['$http', function($http) {
 function routeConfig($routeProvider) {
 	$routeProvider.when('/', {
 		controller: 'homeController',
-		controllerAs: 'homeC',
+		controllerAs: 'home',
 		templateUrl: 'home.html'
 	})
 }
@@ -194,8 +235,40 @@ mod.config(routeConfig);
 
 mod.controller('homeController', ['data', 'utility','$location', function(data, utility, $location){
 	var vm  = this;
+	vm.urlField = {'text': ''};
+	vm.data = data;
+
+	vm.scrapeURL = function () {
+		// Check if input is valid url
+		if (isValidURL(vm.urlField.text)) { 
+
+			// try scrape from import.io
+			utility.scrapeF21(vm.urlField.text);
+			vm.urlField.text = '';
+		}
+	};
 	
-	vm.next = function() {
-		$location.path(utility.findNext('cover'));
+
+	vm.removeItem = function(itemNumber) {
+		index = vm.data.items.length - itemNumber;
+		vm.data.items.splice(index, 1);
 	}
+
+	vm.selectColorForItem = function(color, item) {
+		item.color = color;
+	}
+
+	vm.selectSizeForItem = function(size, item) {
+		item.size = size;
+	}
+	
 }]);
+
+function isValidURL(str) {
+	if (str.indexOf('amazon.com') != -1) {
+		return true;
+	} else if (str.indexOf('forever21.com') != -1) {
+		return true;
+	};
+	return false;
+}
