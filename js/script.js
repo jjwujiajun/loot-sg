ad2fa1bf18a4b4c6d7d65a1840b051e44010a526
Jiajun <jj@liquor.com>
2016-11-07 16:33:13 +0800

    Autofocus on putBom input field when page loads

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-	$("#putBomInput").focus();
+$(document).ready(function(){ 
+	// $("#putBomInput").focus();
 	
     $("#backToTop").on("click", function() {
     	$("body").animate({ scrollTop: '0'});
