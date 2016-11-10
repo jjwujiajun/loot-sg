0bc9175490aa5fd1da9fd0da7713364255b2b4f4
Will Ho <ikaruswill.ho@gmail.com>
2016-11-11 00:49:16 +0800

    Modified query to not increment AUTO_INCREMENT value on every check for duplicate keys. Resolved #41

--- backend/add_update_user.php
+++ backend/add_update_user.php
@@ -35,19 +35,57 @@
         $email          = $userInfo['email'];
         $keep_updated   = $userInfo['keepMeUpdated'];
 
-        $query_string = "INSERT INTO $table_users (first_name, last_name, address_line_1, address_line_2, postal_code, contact, email, keep_updated) VALUES (:first_name, :last_name, :address_line_1, :address_line_2, :postal_code, :contact, :email, :keep_updated) 
-        ON DUPLICATE KEY UPDATE users_id=LAST_INSERT_ID(users_id), first_name = :first_name, last_name = :last_name, address_line_1 = :address_line_1, address_line_2 = :address_line_2, postal_code = :postal_code, contact = :contact, keep_updated = :keep_updated";
-        $query = $db->prepare($query_string);
-        $query->bindParam(':first_name', $first_name, PDO::PARAM_STR);
-        $query->bindParam(':last_name', $last_name, PDO::PARAM_STR);
-        $query->bindParam(':address_line_1', $address_line_1, PDO::PARAM_STR);
-        $query->bindParam(':address_line_2', $address_line_2, PDO::PARAM_STR);
-        $query->bindParam(':postal_code', $postal_code, PDO::PARAM_INT);
-        $query->bindParam(':contact', $contact, PDO::PARAM_STR);
-        $query->bindParam(':email', $email, PDO::PARAM_STR);
-        $query->bindParam(':keep_updated', $keep_updated, PDO::PARAM_BOOL);
-        $query->execute();
 
+        /***************************************************************************
+        This solution UPDATEs if exists, INSERTs only if row with email does not
+        exist without increase the AUTO_INCREMENT value on each check as opposed to
+        INSERT ON DUPLICATE KEY UPDATE which attempts an INSERT operation first
+        causing unncecessary increase of the PRIMARY KEY.
+        
+        Notes:
+            1. 'dual' is a default 1-row temporary table created on-the-fly in MySQL
+            where col = value = SELECT-ed value.
+            2. INSERT INTO SELECT depends on the SELECT-ed table having the exact 
+            column order as specified
+        ****************************************************************************/
+        $update_string = "
+        UPDATE $table_users 
+            SET first_name = :first_name, last_name = :last_name, address_line_1 = :address_line_1, address_line_2 = :address_line_2, postal_code = :postal_code, contact = :contact, keep_updated = :keep_updated
+            WHERE email = :email;";
+
+        $insert_string = "
+        INSERT INTO $table_users (first_name, last_name, address_line_1, address_line_2, postal_code, contact, email, keep_updated) 
+            SELECT :first_name, :last_name, :address_line_1, :address_line_2, :postal_code, :contact, :email, :keep_updated FROM dual
+                WHERE NOT EXISTS (
+                    SELECT * FROM $table_users WHERE email = :email
+                );"; 
+
+        $update = $db->prepare($update_string);
+        $insert = $db->prepare($insert_string);
+
+        $db->beginTransaction();
+
+        $update->bindParam(':first_name', $first_name, PDO::PARAM_STR);
+        $update->bindParam(':last_name', $last_name, PDO::PARAM_STR);
+        $update->bindParam(':address_line_1', $address_line_1, PDO::PARAM_STR);
+        $update->bindParam(':address_line_2', $address_line_2, PDO::PARAM_STR);
+        $update->bindParam(':postal_code', $postal_code, PDO::PARAM_INT);
+        $update->bindParam(':contact', $contact, PDO::PARAM_STR);
+        $update->bindParam(':email', $email, PDO::PARAM_STR);
+        $update->bindParam(':keep_updated', $keep_updated, PDO::PARAM_BOOL);
+        $update->execute();
+
+        $insert->bindParam(':first_name', $first_name, PDO::PARAM_STR);
+        $insert->bindParam(':last_name', $last_name, PDO::PARAM_STR);
+        $insert->bindParam(':address_line_1', $address_line_1, PDO::PARAM_STR);
+        $insert->bindParam(':address_line_2', $address_line_2, PDO::PARAM_STR);
+        $insert->bindParam(':postal_code', $postal_code, PDO::PARAM_INT);
+        $insert->bindParam(':contact', $contact, PDO::PARAM_STR);
+        $insert->bindParam(':email', $email, PDO::PARAM_STR);
+        $insert->bindParam(':keep_updated', $keep_updated, PDO::PARAM_BOOL);
+        $insert->execute();
+
+        $db->commit();
         return $db->lastInsertId();
         // return user_id
     }
