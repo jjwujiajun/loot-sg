fd4adf8ea3ff539d981fde1a8ccf806f05e47a44
Jiajun Wu <jjwu.jiajun@gmail.com>
2017-02-14 23:55:18 +0800

    Added spree countdown timer to home page

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,5 @@
 var mod = angular.module('loot-sg', ['ngRoute']);
 
-
 mod.service('data', function() {
 
 	this.items    = [];
@@ -841,4 +840,34 @@ mod.controller('modifyController', ['data','utility','$location', function(data,
 		vm.pbInputIsShown = !vm.pbInputIsShown;
 	};
 
+}]);
+
+mod.directive('countdown', ['Util', '$interval', function (Util, $interval) {
+    return {
+        restrict: 'A',
+        scope: { date: '@' },
+        link: function (scope, element) {
+            var future;
+            future = new Date(scope.date);
+            $interval(function () {
+                var diff;
+                diff = Math.floor((future.getTime() - new Date().getTime()) / 1000);
+                return element.text(Util.dhms(diff));
+            }, 1000);
+        }
+    };
+}]).factory('Util', [function () {
+    return {
+        dhms: function (t) {
+            var days, hours, minutes, seconds;
+            days = Math.floor(t / 86400);
+            t -= days * 86400;
+            hours = Math.floor(t / 3600) % 24;
+            t -= hours * 3600;
+            minutes = Math.floor(t / 60) % 60;
+            t -= minutes * 60;
+            seconds = t % 60;
+            return days<0? '': days + ' days ' + hours + ' h ' + minutes + ' m ' + seconds + ' s';
+        }
+    };
 }]);
